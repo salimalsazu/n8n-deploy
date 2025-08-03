40675eaa49721756567596296b767c4d8069801f
salimalsazu <sazusalim@gmail.com>
2025-08-04 02:00:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
